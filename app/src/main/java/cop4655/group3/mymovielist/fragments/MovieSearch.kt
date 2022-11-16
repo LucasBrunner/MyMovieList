37229499eb8d04fef07068318e647205d54d18b2
LucasBrunner <lucasbrunner@gmail.com>
2022-11-15 19:43:47 -0500

    implement: movie data in movie search

--- app/src/main/java/cop4655/group3/mymovielist/fragments/MovieSearch.kt
+++ app/src/main/java/cop4655/group3/mymovielist/fragments/MovieSearch.kt
@@ -5,16 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import cop4655.group3.mymovielist.MainActivity
-import cop4655.group3.mymovielist.R
+import cop4655.group3.mymovielist.data.MovieData
+import cop4655.group3.mymovielist.databinding.FragmentMovieSearchBinding
 
 class MovieSearch(main: MainActivity) : MovieAppFragment(main) {
+
+    private var movieData: MovieData? = null
+
+    private var binding: FragmentMovieSearchBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie_search, container, false)
+        binding = FragmentMovieSearchBinding.inflate(layoutInflater, container, false)
+
+        return binding?.root
+    }
+
+    fun setMovie(movieData: MovieData) {
+        this.movieData = movieData
+        binding?.let { b ->
+            b.searchResult.movieTitle.text = movieData.Title
+            b.searchResult.movieYear.text = movieData.Year
+        }
     }
 
     override fun startup() {} // Is run once when the fragment is created
