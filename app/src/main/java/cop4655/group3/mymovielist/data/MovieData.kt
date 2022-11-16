76ab2b7c8d54bd7131f8e4697a280e3cbcd184df
LucasBrunner <lucasbrunner@gmail.com>
2022-11-15 19:43:30 -0500

    add: movie data

---
+++ app/src/main/java/cop4655/group3/mymovielist/data/MovieData.kt
@@ -0,0 +1,29 @@
+package cop4655.group3.mymovielist.data
+
+data class MovieData(
+    var Actors: String?,
+    var Awards: String?,
+    var BoxOffice: String?,
+    var Country: String?,
+    var DVD: String?,
+    var Director: String?,
+    var Genre: String?,
+    var Language: String?,
+    var Metascore: String?,
+    var Plot: String?,
+    var Poster: String?,
+    var Production: String?,
+    var Rated: String?,
+    var Ratings: List<Rating?>?,
+    var Released: String?,
+    var Response: String?,
+    var Runtime: String?,
+    var Title: String?,
+    var Type: String?,
+    var Website: String?,
+    var Writer: String?,
+    var Year: String?,
+    var imdbID: String?,
+    var imdbRating: String?,
+    var imdbVotes: String?
+)
