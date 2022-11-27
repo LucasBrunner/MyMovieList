473205c97a930785e9106247d4d2cc69bb50f2f0
LucasBrunner <lucasbrunner@gmail.com>
2022-11-26 23:28:06 -0500

    amend

--- app/src/main/java/cop4655/group3/mymovielist/data/MyMovieData.kt
+++ app/src/main/java/cop4655/group3/mymovielist/data/MyMovieData.kt
@@ -1,8 +1,19 @@
 package cop4655.group3.mymovielist.data
 
+import java.util.*
+
+/**
+ * If watchList or planList are null then they this movie is not in those list.
+ */
 data class MyMovieData(
     var stars: Int? = null,
     var heart: Boolean = false,
-    var onWatchList: Boolean = false,
-    var onPlanList: Boolean = false
+    /**
+     * If watchList is null then they this movie is not in the watchlist.
+     */
+    var watchList: Calendar? = null,
+    /**
+     * If planList is null then they this movie is not in the plan-to-watch list.
+     */
+    var planList: Calendar? = null
 )
